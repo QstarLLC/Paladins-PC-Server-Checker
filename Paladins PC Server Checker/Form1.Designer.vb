12bc49905325ad8b4100809fb9c5e2139294d8fd
QstarLLC <qstarmacro@gmail.com>
2017-11-02 20:45:36 +0800

    Add files via upload

---
+++ Paladins PC Server Checker/Form1.Designer.vb
@@ -0,0 +1,57 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.tmrChecker = New System.Windows.Forms.Timer(Me.components)
+        Me.status = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'status
+        '
+        Me.status.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.status.Location = New System.Drawing.Point(12, 9)
+        Me.status.Name = "status"
+        Me.status.Size = New System.Drawing.Size(260, 62)
+        Me.status.TabIndex = 0
+        Me.status.Text = "Loading.."
+        Me.status.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 80)
+        Me.Controls.Add(Me.status)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Paladins PC Server Checker"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents tmrChecker As Timer
+    Friend WithEvents status As Label
+End Class
---
+++ Paladins PC Server Checker/Form1.vb
@@ -0,0 +1,33 @@
+﻿Imports System.Net.Sockets
+
+Public Class Form1
+
+    Public Function CheckPaladinsLobby()
+
+        Dim LobbyServer As New TcpClient
+        Try
+
+            ' Ip and Port
+            LobbyServer.Connect("198.255.78.87", 9000)
+
+            ' Server Online
+            status.Text = "Paladins PC is Online."
+            status.ForeColor = Color.Green
+
+        Catch Excep As Exception
+
+            ' Server Offline
+            status.Text = "Paladins PC is Offline."
+            status.ForeColor = Color.Red
+
+            ' Message the user
+            'MsgBox("Login Server is currently offline, please check the forums.")
+
+        End Try
+
+    End Function
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        CheckPaladinsLobby()
+    End Sub
+End Class
